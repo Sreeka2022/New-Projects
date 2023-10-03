70313a46e036510b779b673ffeea0b2c978b4b91
Sreeka2022 <146851943+Sreeka2022@users.noreply.github.com>
2023-10-04 00:17:21 +0530

    Add files via upload

---
+++ READ_ME.txt
@@ -0,0 +1,7 @@
+The REST API creates an object, and thereafter sends the values of an object in response to the client. It breaks down a transaction in order to create small modules.
+ Now, each of these modules is used to address a specific part of the transaction.
+
+
+Please note that all of the projects (including the app development) were done 
+in a tight time constraint
+
---
+++ subscribers.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const router = express.Router()
+const Subscriber = require('../models/subscriber')
+const subscriber = require('../models/subscriber')
+
+
+//Getting all
+router.get('/', async(req,res) => {
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
+
+//Getting One
+router.get('/:id',getSubscriber,(req,res) => {
+    res.json(res.subscriber)
+})
+//Creating One
+router.post('/', async (req,res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+try{
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+}
+catch(err){
+    res.status(400).json({message: err.message})
+}
+})
+//Updating One 
+router.patch('/:id',getSubscriber,async (req,res) => {
+    if(req.body.name !=null){
+        res.subscriber.name =req.body.name
+    }
+    if(req.body.subscribedToChannel!=null){
+        res.subscriber.name =req.body.subscribedToChannel}
+    try{
+        const updatedSubScriber = await res.subscriber.save()
+        res.json(updatedSubScriber)
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
+}
+)
+//Deleting One
+router.delete('/:id',getSubscriber,async (req,res) => {
+    try{
+        await res.subscriber.remove()
+        res.json({message: 'Deleted Subscriber'})
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
+
+async function getSubscriber(req,res,next){
+    let subscriber
+    try{
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null){
+            return res.status(404).json({message: 'Cannot find Subscriber'})
+        }
+    }
+    catch(err){
+            return res.status(500).json({message:err.message})
+    }
+    res.subscriber = subscriber
+    next()
+} 
+module.exports = router
